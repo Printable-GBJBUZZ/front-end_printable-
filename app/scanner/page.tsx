d2743b29539f03ef8df1f61b5260e094006a6fd6
Rajat Rajput <rajat.r1024@gmail.com>
2025-07-21 23:26:04 +0530

    Scanner page

---
+++ app/scanner/page.tsx
@@ -0,0 +1,22 @@
+import Scanner from "@/components/scanner/scan";
+
+export default function ScannerPage() {
+  return (
+      <Scanner />
+
+  );
+}
+
+
+
+// import Scanner from "@/components/scanner/scan";
+// import NewNavBar from "@/components/scanner/navbar";
+
+// export default function ScannerPage() {
+//   return (
+//     <>
+//       <NewNavBar />
+//       <Scanner />
+//       </>
+//   );
+// }
